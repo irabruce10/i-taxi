48228d1abebc717c211ba272828cc7ce25033567
Bruce <irabruce10@gmail.com>
2025-02-22 00:01:07 +0100

    I taxi expo project

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,13 @@
 import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { router } from 'expo-router';
+import Swiper from 'react-native-swiper';
+import { useRef, useState } from 'react';
 
 export default function onBoarding() {
+  // TODO: Implement Swiper component for onboarding slides and navigation buttons.
+  const swiperRef = useRef<Swiper>(initialValue:null);
+  const [activeIndex,setActiveIndex]=  useState(0)
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -14,6 +18,17 @@ export default function onBoarding() {
       >
         <Text className=" text-black text-md font-JakartaBold ">Skip</Text>
       </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]" />}
+        activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#3182CE] rounded-full" />}
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+
+        [{}]
+      </Swiper>
     </SafeAreaView>
   );
 }
