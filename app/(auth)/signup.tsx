3f4c2faaa03fdec2e5a251d035c8302ee01aa4ad
Bruce <irabruce10@gmail.com>
2025-02-24 11:23:02 +0100

    I taxi expo project

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,11 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React from 'react';
+import { images } from '../../constants';
 
 export default function signup() {
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create your Account
+          </Text>
+        </View>
+        <View className="p-5"></View>
+      </View>
+      {/* Add signup form components here */}
+    </ScrollView>
   );
 }
 
